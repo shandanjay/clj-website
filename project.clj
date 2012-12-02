9c2ab132fa218b8a25254462500170760981c40c
Eduardo Raad <eduardoraad@gmail.com>
2012-12-01 21:24:19 -0500

    Alfa.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-website/lein-template "0.1.0-SNAPSHOT"
   :description "A leiningen template to create heroku-ready web app projects using Noir, Enlive and Twitter Bootstrap."
-  :url "http://eraad.me"
+  :url "https://github.com/eraad/clj-website"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
